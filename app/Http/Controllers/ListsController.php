dddcaf22be01f3d383b27b58045115821c3aa77d
Bram Slob <bramslob@gmail.com>
2017-05-26 15:16:21 +0200

    General direction for getting stuff from API

--- app/Http/Controllers/ListsController.php
+++ app/Http/Controllers/ListsController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Client;
 use Illuminate\Routing\Controller as BaseController;
 
 class ListsController extends BaseController
 {
-    public function index()
+    /**
+     * @param Client $client
+     * @return \Illuminate\View\View
+     */
+    public function index(Client $client)
     {
+        $response = $client->get('http://slobshop-api.dev/api/v1/lists');
+
+        dd($response);
+
         return view('Lists.index');
     }
 }
